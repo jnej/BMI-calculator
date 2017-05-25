0c12428287c4764d12f58144573ba3e904ddd26f
Amanda Johnson <amandajnej@gmail.com>
2017-05-25 18:22:49 +0100

    Add initial code

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var bmi = function(weight,height) { return weight/(height*height);};
+
+var weight=parseFloat(prompt("enter weight in kilograms"));
+
+var height = parseFloat(prompt("enter height in meter"));
+
+var result = bmi(weight,height);
+
+alert(result);
+
+//add, subtract, multiply, and divide.
